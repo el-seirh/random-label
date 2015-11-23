8220879731dda9bceb58403db3fbc22bd1346e1c
Max Gfeller <mg@cyon.ch>
2015-11-23 18:19:55 +0100

    Initial commit

---
+++ randomlabel.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+WORDS1=(schnäbi schnuderi plapper stai schlabber chäller schnägge furz kack huffe stink hoode furzkopf schnudder arsch angge schnaabel drottwaaramsle fümflyyber gnulleri maitli käpseli sagg schwööbli schyssdrägg)
+WORDS2=(schliffer chasper chätscher gwaggli suuri pfluume schlägger trambbler fürzler chaib bolle molch bohne kopf burscht wurscht molch tante joodler mäitli bueb schmegger ladäärnli spalter laddärne schugger ryter)
+echo ${WORDS1[$(( ((RANDOM<<15)|RANDOM) % ${#WORDS1[@]} + 1 ))]}${WORDS2[$(( ((RANDOM<<15)|RANDOM) % ${#WORDS2[@]} + 1 ))]}
